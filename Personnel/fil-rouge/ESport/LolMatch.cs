0369a1e03815f862a5e0825795a658083ce042d0
erdemcasa <pl23ksc@eduvaud.ch>
2026-09-02 11:22:49 +0200

    feat(lolmatch): Correction constructeur
    [10][DONE]

--- Personnel/fil-rouge/ESport/LolMatch.cs
+++ Personnel/fil-rouge/ESport/LolMatch.cs
@@ -7,11 +7,12 @@ namespace ESport
 {
     public class LolMatch
     {
-        public LolMatch(string player, string champion, string role, int deaths, int assists, int cs, int visionScore, bool won)
+        public LolMatch(string player, string champion, string role, int kills, int deaths, int assists, int cs, int visionScore, bool won)
         {
             Player = player;
             Champion = champion;
             Role = role;
+            Kills = kills;
             Deaths = deaths;
             Assists = assists;
             CS = cs;
@@ -22,6 +23,7 @@ namespace ESport
         public string Player { get; }
         public string Champion { get; }
         public string Role { get; }
+        public int Kills { get; }
         public int Deaths { get; }
         public int Assists { get; }
         public int CS { get; }
