ee0afb3a5b12e0dd01d0f9554cedcd6ca7a70fed
erdemcasa <pl23ksc@eduvaud.ch>
2026-09-02 11:23:38 +0200

    feat(program): Plus de hardcode, utilisation CSV.
    [25][DONE]

--- Personnel/fil-rouge/ESport/Program.cs
+++ Personnel/fil-rouge/ESport/Program.cs
@@ -1,20 +1,74 @@
 ﻿using DataSeries;
 using ESport;
 
-var valorant = DataSeries<ValorantMatch>.From(new[]
-{
-    new ValorantMatch("Léa", "Jett", 18, 6, 4, 8, 13, true),
-    new ValorantMatch("Léa", "Reyan", 18, 6, 4, 8, 13, true),
-});
+DataSeries<DataPoint<ValorantMatch>> valorant;
+DataSeries<DataPoint<Cs2Match>> cs2;
+DataSeries<DataPoint<LolMatch>> lol;
 
-var lol = DataSeries<LolMatch>.From(new[]
-
-{
-    new LolMatch("Noé", "Thresh", "Support", 7, 4, 8, 11, false),
-    new LolMatch("Noé", "Thresh", "Support", 7, 4, 8, 11, false),
-    new LolMatch("Noé", "Thresh", "Support", 7, 4, 8, 11, false),
-});
-
+valorant = DataSeries <DataPoint<ValorantMatch>>.FromCsv("./data/valorant.csv", ParseValorant);
+lol = DataSeries<DataPoint<LolMatch>>.FromCsv("./data/lol.csv", ParseLol);
+cs2 = DataSeries<DataPoint<Cs2Match>>.FromCsv("./data/cs2.csv", ParseCs2);
 
 Console.WriteLine($"Valorant match: {valorant.Count}");
 Console.WriteLine($"LoL match: {lol.Count}");
+Console.WriteLine($"CS2 match: {cs2.Count}");
+
+
+Environment.Exit(67);
+
+
+DataPoint<ValorantMatch> ParseValorant(string[] cols)
+{
+    ValorantMatch match = new ValorantMatch(
+        cols[1], // Player
+        cols[2], // Agent
+        int.Parse(cols[3]), // Kills
+        int.Parse(cols[4]), // Deaths
+        int.Parse(cols[5]), // Assists
+        int.Parse(cols[6]), // Headshots
+        int.Parse(cols[7]), // Damage
+        bool.Parse(cols[8])  // Win
+    );
+    
+    DateTime date = DateTime.Parse(cols[0]); // Date
+
+    return new DataPoint<ValorantMatch>(date, match);
+}
+
+DataPoint<LolMatch> ParseLol(string[] cols)
+{
+    LolMatch match = new LolMatch(
+        cols[1], // Player
+        cols[2], // Champion
+        cols[3], // Role
+        int.Parse(cols[4]), // Kills
+        int.Parse(cols[5]), // Deaths
+        int.Parse(cols[6]), // Assists
+        int.Parse(cols[7]), // CS
+        int.Parse(cols[8]), // Vision Score
+        bool.Parse(cols[9])  // Win
+    );
+    
+    DateTime date = DateTime.Parse(cols[0]); // Date
+
+    return new DataPoint<LolMatch>(date, match);
+}
+
+
+DataPoint<Cs2Match> ParseCs2(string[] cols)
+{
+    Cs2Match match = new Cs2Match(
+        cols[1], // Player
+        cols[2], // Map
+        cols[3], // Side
+        int.Parse(cols[4]), // Kills
+        int.Parse(cols[5]), // Deaths
+        int.Parse(cols[6]), // Assists
+        int.Parse(cols[7]), // Headshots
+        bool.Parse(cols[8])  // Win
+    );
+    
+    DateTime date = DateTime.Parse(cols[0]); // Date
+
+    return new DataPoint<Cs2Match>(date, match);
+}
