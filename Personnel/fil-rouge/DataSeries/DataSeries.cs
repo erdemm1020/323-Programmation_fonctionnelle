9cf05fb5fd2dea01e7965527507e19b7ef578fd6
erdemcasa <pl23ksc@eduvaud.ch>
2026-09-02 10:48:47 +0200

    feat(DataSeries): Ajout fonction FromCsv
    [15][DONE]

--- Personnel/fil-rouge/DataSeries/DataSeries.cs
+++ Personnel/fil-rouge/DataSeries/DataSeries.cs
@@ -14,6 +14,12 @@ namespace DataSeries
         public static DataSeries<T> From(IEnumerable<T> source)
             => new DataSeries<T>(source);
 
+        public static DataSeries<T> FromCsv(string path, Func<string[], T> parser) 
+        {
+            var lines = File.ReadAllLines(path).Skip(1);
+            return new DataSeries<T>(lines.Select(line => parser(line.Split(','))));
+        }
+
         public int Count => _data.Count();
         public IEnumerable<T> Values => _data;
     }
